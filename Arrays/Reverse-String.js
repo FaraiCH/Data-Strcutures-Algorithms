905ff3d781fed6a614ebb2cce30e765b60b71cc4
Farai Chakarisa <fmc@bt-industrial.co.za>
2025-01-14 11:55:59 +0200

    Reverse String

--- Arrays/Reverse-String.js
+++ Arrays/Reverse-String.js
@@ -9,14 +9,30 @@ function reverse(str)
         return "Wrong man";
     }
 
+    // Create the array to store the characters
     let backwards = [];
 
+    // Get the length on the string
     const total = str.length - 1;
 
-    for(let i = total; i = 0; i--)
+    // Decrement loop.
+    for(let i = total; i >= 0; i--)
     {
+        // No need for array split. JavaScript can split it just fine.
         backwards.push(str[i]);
     }
 
     return backwards.join('');
 }
+
+function optimalReverse(str)
+{
+    // Using Javascript methods
+    return str.split('').reverse().join('');
+}
+
+// Can use this spreed operator and arrow function too
+const reverseOneline = str => [...str].reverse().join('')
+
+console.log(reverse("I am the terminator"));
+console.log(optimalReverse("I am the terminator"));
